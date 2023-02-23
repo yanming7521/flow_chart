290b4a1aa5dc37224f68d17f2f930052155e114a
yanming <yanminng@126.com>
2023-02-23 17:25:03 +0800

    去除日志

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = defineConfig({
   publicPath: './',
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            // 删除注释
+            output: {
+              comments: false
+            },
+            compress: {
+              drop_console: true, //删除所有调式带有console的
+              drop_debugger: true,
+              pure_funcs: ['console.log'] //删除console.log
+            }
+          }
+        })
+      ]
+    },
+  },
 })
